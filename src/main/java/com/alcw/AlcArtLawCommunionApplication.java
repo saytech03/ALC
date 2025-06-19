5b2fc3bb3b69771ed69beeb2b3d1dbec255948e2
deb2025 <debdutta.basu.2020@gmail.com>
2025-06-19 19:55:16 +0530

    new changes for render deployment

--- src/main/java/com/alcw/AlcArtLawCommunionApplication.java
+++ src/main/java/com/alcw/AlcArtLawCommunionApplication.java
@@ -1,5 +1,6 @@
 package com.alcw;
 
+import org.parse4j.Parse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
